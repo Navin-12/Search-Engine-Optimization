179799ca55ec16db04b5aeeb1b36b21b7d4a7d7f
Navin-12 <55185267+Navin-12@users.noreply.github.com>
2020-06-18 19:15:54 +0530

    Add files via upload

---
+++ test prog.py
@@ -0,0 +1,51 @@
+#factors
+'''
+a=int(input('enter the number'))
+for i in range(1,a+1):
+    if(a%i==0):
+        print(i) '''
+#sum of factors
+'''
+a=int(input('enter the number'))
+sum=0
+for i in range(1,a+1):
+    if(a%i==0):
+        sum=sum+i
+print(sum)'''
+#sum of n numbers
+'''
+a=int(input('enter the number'))
+sum=0
+for i in range(1,a+1):
+    sum=sum+i
+print(sum)'''
+#factorial numbers
+'''
+a=int(input('enter the number'))
+f=1
+for i in range(1,a+1):
+    f=f*i
+print(f)'''
+#Even number
+'''
+a=int(input('enter the number'))
+b=int(input('enter the number'))
+for c in range(a,b+1):
+    if(c%2==0):
+        print('even number',c)
+'''
+#odd number
+'''
+a=int(input('enter the number'))
+b=int(input('enter the number'))
+for c in range(a,b+1):
+    if(c%2==1): #(c!=0)
+        print('odd number',c)
+'''
+
+
+
+
+
+
+
